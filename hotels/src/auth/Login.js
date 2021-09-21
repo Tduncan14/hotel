f9be75f6657e55e79bc1ae5180f86674c10350cf
tduncan14 <nbatre1@yahoo.com>
2021-09-21 14:27:41 -0700

    keep moving forward

--- hotels/src/auth/Login.js
+++ hotels/src/auth/Login.js
@@ -1,20 +1,36 @@
 import {toast} from 'react-toastify';
 import {login} from '../actions/auth';
-
+import LoginForm from '../components/LoginForm';
+import { useState } from 'react';
 
 const Login = () => {
 
+
+    const [email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+
+    }
 
 
 
     return(
         <>
         <div className="container-fluid bg-secondary p-5 text-center">
+           <h1>Login</h1>
+        </div>
 
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <LoginForm />
 
+                </div>
 
- 
-            
+            </div>
         </div>
 
         </>
